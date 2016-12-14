4a6921944fd1bc392f0dc2762580782d7f30ea7e
Bryce Cordill <brycecordill@gmail.com>
2016-12-14 13:47:08 -0800

    Most of code for full autonomous with beacons (NEEDS TESTING)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Automagical_RED3650.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Automagical_RED3650.java
@@ -19,10 +19,11 @@ public class Automagical_RED3650 extends LinearOpMode{
     ColorSensor colorSensor;
     OpticalDistanceSensor ods;
     LightSensor light;
-    Servo colorServo;
     DcMotor lDrive, rDrive, collector, shooter;
     double lThresh, dThresh;
 
+
+    //TODO: add new servos once they are in robot
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -34,70 +35,101 @@ public class Automagical_RED3650 extends LinearOpMode{
         rDrive = hardwareMap.dcMotor.get("rDrive");
         collector = hardwareMap.dcMotor.get("collector");
         shooter = hardwareMap.dcMotor.get("shooter");
-        colorServo = hardwareMap.servo.get("colorServo");
+
         colorSensor = hardwareMap.colorSensor.get("colorSensor");
         ods = hardwareMap.opticalDistanceSensor.get("ods");
         colorSensor.enableLed(false);
         light = hardwareMap.lightSensor.get("light");
 
-        //backwards is forwards
+
         rDrive.setDirection(DcMotor.Direction.REVERSE);
-        colorServo.setPosition(1.00);
+
         //shooter.setDirection(DcMotor.Direction.REVERSE);
 
 
 
         waitForStart(); //waits for start button to be pressed
 
-        rDrive.setPower(-.25);
-        lDrive.setPower(-.25);
+        lDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        while (light.getLightDetected() < lThresh){
+        rDrive.setTargetPosition(rDrive.getCurrentPosition()+1100);
+        lDrive.setTargetPosition(lDrive.getCurrentPosition()+1100);
+        rDrive.setPower(.4);
+        lDrive.setPower(.4);
+
+        //spin up shooter
+        shooter.setPower(1.00);
+
+        Thread.sleep(2500);
+
+        lDrive.setPower(0);
+        rDrive.setPower(0);
+
+        //start shooting
+        collector.setPower(-1.00);
+        Thread.sleep(1000);
+
+        //wait for shooter to speed up again
+        collector.setPower(0);
+        shooter.setPower(.4);
+        Thread.sleep(1000);
+        shooter.setPower(0);
+
+        rDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //backwards is forwards for some reason
+        lDrive.setPower(-.5);
+        rDrive.setPower(-.5);
+
+        while(ods.getLightDetected() <= 0){
+            continue;
+        }
+
+        lDrive.setPower(0);
+        rDrive.setPower(0);
+
+        Thread.sleep(750);
+
+        rDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        rDrive.setTargetPosition(rDrive.getCurrentPosition()-1200);
+        lDrive.setTargetPosition(lDrive.getCurrentPosition()+1200);
+        rDrive.setPower(.5);
+        lDrive.setPower(.5);
+        Thread.sleep(2000);
+
+        rDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        rDrive.setPower(.3);
+        lDrive.setPower(.3);
+
+        while(light.getLightDetected() < lThresh){
             continue;
         }
         rDrive.setPower(0);
         lDrive.setPower(0);
-        Thread.sleep(750);
 
-        while (light.getLightDetected() < lThresh){
-          rDrive.setPower(.25);
+        if(colorSensor.red() > colorSensor.blue() && colorSensor.red() > 1){
+            //hit button with servo
+            Thread.sleep(1000);
+            //bring back servo
+        }
+        else if(colorSensor.blue() >= colorSensor.red()){
+            //hit button with other servo
+            Thread.sleep(1000);
+            //bring servo back
+        }
+        else{
+            //run away
         }
 
-        while (ods.getLightDetected() < dThresh){
-          double correction = (perfectLight - light.getLightDetected());
+        //check to see how much time is left
+        //if enough left, go for next beacon
 
-          if(correction <= 0 ){
-            lDrive.setPower(.25);
-            rDrive.setPower(.25 - correction);
-          }
-          else {
-            lDrive.setPower(.25 + correction);
-            rDrive.setPower(.25);
-          }
-        }
-        rDrive.setPower(0);
-        lDrive.setPower(0);
-
-        Thread.sleep(750);
-
-        if(colorSensor.red() > colorSensor.blue()){
-          rDrive.setPower(.3);
-          lDrive.setPower(.3);
-        }
-        else {
-          colorServo.setPosition(-1.00);
-          Thread.sleep(750);
-          rDrive.setPower(.3);
-          lDrive.setPower(.3);
-        }
-
-        Thread.sleep(250);
-        rDrive.setPower(-1.00);
-        lDrive.setPower(-1.00);
-        Thread.sleep(500);
-
-        rDrive.setPower(0);
-        lDrive.setPower(0);
 
 
 
