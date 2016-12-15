8e3053c273db1d5be4267ec578556c5a163cec37
Bryce Cordill <brycecordill@gmail.com>
2016-12-14 21:03:17 -0800

    Added Wenlong's TeleOp code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp9367.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+/**
+ * Created by williamhuang on 14/12/16.
+ */
+
+//TODO: put more comments!! I have no idea what is going on
+//I think you need to have different names in .get for the different drives
+//-Bryce
+
+@TeleOp(name="Operation: Telly", group="9367")
+public class TeleOp9367 extends OpMode{
+    //assigning state variables
+    DcMotor rfDrive, lfDrive, rrDrive, lrDrive;
+
+
+
+    @Override
+    public void init() {
+
+        // linking variables to hardware components
+        lfDrive = hardwareMap.dcMotor.get("lDrive");
+        rfDrive = hardwareMap.dcMotor.get("rDrive");
+        lrDrive = hardwareMap.dcMotor.get("lDrive");
+        rrDrive = hardwareMap.dcMotor.get("rDrive");
+
+        //Check if drive names ok, may have conflicts ^
+
+
+
+    }
+
+    @Override
+    public void loop() {
+
+        if(gamepad1.dpad_left){
+            lfDrive.setPower(1.0);
+            lrDrive.setPower(-1.0);
+            rfDrive.setPower(-1.0);
+            rrDrive.setPower(1.0);
+        }
+
+        if(gamepad1.dpad_right){
+            lfDrive.setPower(-1.0);
+            lrDrive.setPower(1.0);
+            rfDrive.setPower(1.0);
+            rrDrive.setPower(-1.0);
+        }
+
+        if(gamepad1.dpad_up){
+            lfDrive.setPower(1.0);
+            lrDrive.setPower(1.0);
+            rfDrive.setPower(1.0);
+            rrDrive.setPower(1.0);
+        }
+
+        if(gamepad1.dpad_down){
+            lfDrive.setPower(-1.0);
+            lrDrive.setPower(-1.0);
+            rfDrive.setPower(-1.0);
+            rrDrive.setPower(-1.0);
+        }
+
+        lfDrive.setPower(-gamepad1.left_stick_y+0.5*gamepad1.left_stick_x);
+        lrDrive.setPower(-gamepad1.left_stick_y+0.5*gamepad1.left_stick_x);
+        rfDrive.setPower(gamepad1.left_stick_y+0.5*gamepad1.left_stick_x);
+        rrDrive.setPower(gamepad1.left_stick_y+0.5*gamepad1.left_stick_x);
+
+
+    }
+
+}
