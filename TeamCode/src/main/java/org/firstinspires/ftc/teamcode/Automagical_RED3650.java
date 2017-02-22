d2618c0345b55f7099dc21aad2a418788944da6c
Bryce Cordill <brycecordill@gmail.com>
2017-02-22 09:46:08 -0800

    Starting new code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Automagical_RED3650.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Automagical_RED3650.java
@@ -4,9 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.LightSensor;
-import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
